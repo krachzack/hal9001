7358502efb958b9839b1b979e1c4cbfc3cb23a99
krachzack <hello@phstadler.com>
2017-03-27 19:09:03 +0200

    Slightly better ramming logic

--- src/im500/main/HAL9001.java
+++ src/im500/main/HAL9001.java
@@ -3,6 +3,7 @@ package im500.main;
 import java.awt.Color;
 
 import robocode.AdvancedRobot;
+import robocode.HitRobotEvent;
 import robocode.HitWallEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
@@ -20,6 +21,7 @@ public class HAL9001 extends AdvancedRobot {
 		
 		while (true) {
 			setTurnRadarRight(Double.POSITIVE_INFINITY);
+			
 			execute();
 		}
 	}
@@ -28,8 +30,18 @@ public class HAL9001 extends AdvancedRobot {
 	public void onScannedRobot(ScannedRobotEvent event) {
 		setTurnRight(event.getBearing());
 		execute();
-		setFire(10.0);
+		if(event.getDistance() < 5) {
+			setFire(3.0);
+		}
 		setAhead(event.getDistance() + 5);
+		execute();
+		scan();
+	}
+	
+	@Override
+	public void onHitRobot(HitRobotEvent event) {
+		setAhead(-2);
+		setFire(10.0);
 		execute();
 		scan();
 	}
