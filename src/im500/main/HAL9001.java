92455f197169b4aaf77041aa198ee5d8b894ff95
krachzack <hello@phstadler.com>
2017-03-28 21:52:11 +0200

    Integrated pattern matcher into HAL9001

--- src/im500/main/HAL9001.java
+++ src/im500/main/HAL9001.java
@@ -12,6 +12,7 @@ public class HAL9001 extends AdvancedRobot {
 	private static int CLOSE_QUARTERS_DISTANCE = 90;
 	private Scanner scanner = new Scanner(this);
 	private Tracker tracker = new Tracker(this);
+	private PatternMatcher matcher = new PatternMatcher(this);
 
 	private void initialize() {
 		setColors(Color.BLACK, Color.RED, Color.YELLOW, Color.PINK, Color.BLACK);
@@ -32,6 +33,7 @@ public class HAL9001 extends AdvancedRobot {
 	public void onScannedRobot(ScannedRobotEvent event) {
 		tracker.onScannedRobot(event);
 		scanner.onScanned(event);
+		matcher.onScanned(event);
 		
 		Move.towards(this, tracker.getProjectedX(0), tracker.getProjectedY(0));
 	
@@ -49,13 +51,23 @@ public class HAL9001 extends AdvancedRobot {
 		double bulletVelocity = 20 - 3 * firepower;
 		double ticksUntilHit = event.getDistance() / bulletVelocity;
 		System.out.println("Ticks: " + ticksUntilHit);
-				
-		double overExtapolation = 1.0;
-		Move.aim(this, tracker.getProjectedX(ticksUntilHit*overExtapolation), tracker.getProjectedY(ticksUntilHit*overExtapolation));
+		
+		double[] ememyPosFutureDumb = new double[] { tracker.getProjectedX(ticksUntilHit), tracker.getProjectedY(ticksUntilHit) };
+		double[] enemyPosFutureSmart = matcher.getFuturePosition(
+			(int) Math.round(ticksUntilHit),
+			ememyPosFutureDumb,
+			event.getHeadingRadians()
+		);
+		double[] enemyPosFuture = (enemyPosFutureSmart == null) ? ememyPosFutureDumb : enemyPosFutureSmart;
+		
+		Move.aim(this, enemyPosFuture[0], enemyPosFuture[1]);
 		getGraphics().setColor(Color.GREEN);
-		getGraphics().drawLine((int)getX(), (int)getY(), (int)tracker.getProjectedX(0), (int)tracker.getProjectedY(0));
-		getGraphics().setColor(Color.RED);
-		getGraphics().drawLine((int)getX(), (int)getY(), (int)tracker.getProjectedX(ticksUntilHit*overExtapolation), (int)tracker.getProjectedY(ticksUntilHit*overExtapolation));
+		getGraphics().drawLine((int)getX(), (int)getY(), (int)ememyPosFutureDumb[0], (int)ememyPosFutureDumb[1]);
+		
+		if(enemyPosFutureSmart != null) {
+			getGraphics().setColor(Color.RED);
+			getGraphics().drawLine((int)getX(), (int)getY(), (int)enemyPosFutureSmart[0], (int)enemyPosFutureSmart[1]);
+		}
 		
 		/*setTurnRight(event.getBearing());
 		setTurnGunRight((getHeading() - getGunHeading() + event.getBearing()) % 360);
